1964220c138d824ca883371fa3bf5b0932147760
Talal <82117611+talalaslam15@users.noreply.github.com>
2025-08-03 22:04:52 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Chessboard from "./ChessBoard.tsx";
+import ChessBoard from "./components/ChessBoard.tsx";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-  <Chessboard />
+  <ChessBoard />
   // </StrictMode>
 );
